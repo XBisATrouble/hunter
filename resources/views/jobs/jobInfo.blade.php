21e87c0607f9b9985178c22fe069d66516b9ace9
CrazyGrin <894355833@qq.com>
2017-05-08 18:29:31 +0800

    .

--- resources/views/jobs/jobInfo.blade.php
+++ resources/views/jobs/jobInfo.blade.php
@@ -2,6 +2,10 @@
 <script type="text/javascript" src="/js/city.js"></script>
 @section('content')
 <div class="main_content">
-	<div class="job_nav"></div>
+	<div class="job_nav">
+	</div>
+	<div class="job_des">
+	</div>
+	<div class="job_company"></div>
 </div>
 @endsection('content')
