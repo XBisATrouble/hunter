a6de6391bb59527fbab0b3d441c5175ad601a1c2
CrazyGrin <894355833@qq.com>
2017-05-09 12:45:58 +0800

    .

--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -2,10 +2,27 @@
 <script type="text/javascript" src="/js/city.js"></script>
 @section('content')
 <div class="main_content">
-	<div class="job_nav">
+	<div class="job_des_moduel moduel">
+		<div class="job_simple_des">
+			<div class="job_simple_des_tag">
+				<p class="job_name">PHP工程师</p>
+				<p class="job_from">Hunter</p>
+				<div class="job_simple_des_tag_tan"></div>
+			</div>
+			<p class="job_salary"><img src="/dist/sal.png">8K-12K</p>
+			<p class="job_location"><img src="/dist/loc.png">04 0404</p>
+			<p class="job_experience"><img src="/dist/exp.png">应届</p>
+			<p class="job_education"><img src="/dist/edu.png">本科</p>
+			<div class="line"></div>
+			<p class="job_description">
+				岗位职责：\r\n\r\n1. 熟练掌握PHP+MySQL编程,具备独立开发网站能力； \r\n\r\n2. 熟练掌握MySQL数据库常规操作和简单数据库优化，熟练编写高效率的sql语句；\r\n\r\n3. 熟练掌握 WEB 前端开发技术html5、js、css3、ajax；\r\n\r\n4. 参与公司项目开发工作，参与代码编写，测试和bug修复工作； \r\n\r\n5. 负责前后台项目功能模块的编程、测试任务，确保项目的进度和质量； \r\n\r\n7. 与UED中心UE/UI设计人员保持良好沟通，能快速理解、消化各方需求；\r\n\r\n \r\n\r\n任职要求：\r\n\r\n1. 本科及以上学历，3年以上相关工作经验（除实习期和培训机构做项目以外的正式工作）； \r\n\r\n2. 精通PHP, MySQL, Linux, 练掌握jQuery，熟悉PHP smarty模板技术或框架技术； \r\n\r\n3．最后一家公司非外包公司的优先；\r\n\r\n4．有网络小说或内容分发平台开发经验的优先；\r\n\r\n5．带过五人及以上团队的优先。
+			</p>
+			<div class="line"></div>
+			<div class="job_company_description">
+				
+			</div>
+		</div>
+		<div class="job_hr_banner"></div>
 	</div>
-	<div class="job_des">
-	</div>
-	<div class="job_company"></div>
 </div>
 @endsection('content')
