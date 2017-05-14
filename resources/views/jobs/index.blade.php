ba91b0af131a30b524c5b73aab11c6e3ec0b97b1
CrazyGrin <894355833@qq.com>
2017-05-14 14:43:38 +0800

    cs

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -156,19 +156,24 @@
 		@foreach($jobs as $job)
 		<div class="index_single_item">
 			<div class="index_single_item_jobinfo orange">
-				<div class="index_single_item_jobinfo_salary">18k-10k</div>
-				<div class="per50">
-					<a class="blue big" href="jobs/{{$job->id}}">{{ $job->name }}</a>
-					<p class="gray">
-						<span class="gray">工作发布于</span> : {{ $job->created_at }}
-					</p>
-					<p class="gray" style="height: 88px;">
-						<span class="gray">工作简介</span>
-						:
-						<br>{!! mb_substr($job->description,0,150,"utf-8").'...' !!}</p>
+				<div class="per50 right_padding">
+					<div class="per50">
+						<a class="big blue" href="/">{{ $job->name }}</a>
+						<p class="gray margin15">公司名称 : {{ $job->publish->name }}</p>
+						<p class="gray margin15">学历要求 : {{ $job->education }}</p>
+						<p class="gray margin15">工作地点 : {{ $job->province }}</p>
+					</div>
+					<div class="per50">
+						<div class="user_main_single_jobinfo_button">工作详情</div>
+						<p class="orange margin15">薪资 : {{ $job->salary }}</p>
+						<p class="gray margin15">工作经验 : {{ $job->experience }}</p>
+						<p class="gray margin15">发布时间 : {{ $job->updated_at }}</p>
+					</div>
 				</div>
-				<div class="per50">
-					<a class="blue big text_align_r" href="">恽涵有限公司</a>
+				<div class="per50 left_border left_padding">
+					<a class="blue big text_align_r" href="">{{ $job->publish->name }}</a>
+					<div class="index_single_item_jobinfo_company_avatar"></div>
+					<div class="index_single_item_jobinfo_company_info"></div>
 				</div>
 			</div>
 		</div>
