8a9809037a9aa5e2c6e5655a88b32f5ca35d2220
CrazyGrin <894355833@qq.com>
2017-05-15 18:01:04 +0800

    csss

--- public/js/user_function.js
+++ public/js/user_function.js
@@ -1,5 +1,17 @@
-(function search_listener(){
-	document.querySelector('.search_button_trigger').addEventListener('click',()=>{
+(function search_listener() {
+	document.querySelector('.search_button_trigger').addEventListener('click', () => {
 		document.querySelector('.serach_button_submit').click();
+	});
+})();
+
+(function check_box_function() {
+	var trigger = document.querySelector('.checkcube');
+
+	trigger.addEventListener('click', (e) => {
+		if (e.target.className = "checkcube") {
+			e.target.className = "checkcube checkcube_checked";
+		}else{
+			e.target.className = "checkcube";
+		}
 	});
 })();
