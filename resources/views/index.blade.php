721a1b65b90558a3fdbc4996d8deec531bd24675
XBisATrouble <XBisATrouble@gmail.com>
2017-05-16 23:53:28 +0800

    职业筛选页面

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -61,19 +61,4 @@
 
 		</div>
 	</div>
-	@if (Session::has('sweet_alert.alert'))
-		<script>
-            swal({
-                text: "{!! Session::get('sweet_alert.text') !!}",
-                title: "{!! Session::get('sweet_alert.title') !!}",
-                timer: "{!! Session::get('sweet_alert.timer') !!}",
-                type: "{!! Session::get('sweet_alert.type') !!}",
-                showConfirmButton: "{!! Session::get('sweet_alert.showConfirmButton') !!}",
-                confirmButtonText: "{!! Session::get('sweet_alert.confirmButtonText') !!}",
-                confirmButtonColor: "#AEDEF4"
-
-                // more options
-            });
-		</script>
-	@endif
 @endsection
