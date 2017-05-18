d332c60a9dfed733864a9590f698d0269dc33ebe
CrazyGrin <894355833@qq.com>
2017-05-18 17:08:26 +0800

    fix byte overflow with using white-space nowrap

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -41,16 +41,16 @@
 			<div class="user_single_item_jobinfo">
 				<div class="per50">
 					<a class="big blue" href="jobs/{{ $job->id }}">{{ $job->name }}</a>
-					<p class="gray margin15">公司名称 : {{ $job->publish->name }}</p>
+					<p class="gray margin15">公司 : {{ $job->publish->name }}</p>
 					<p class="gray margin15">学历要求 : {{ $job->education }}</p>
-					<p class="gray margin15">工作地点 : {{ $job->province }}</p>
+					<p class="gray margin15">工作所在地 : {{ $job->province }}</p>
 				</div>
 				<div class="per50">
 					<div class="user_main_single_jobinfo_button">
 						<a href="jobs/{{ $job->id }}">工作详情</a>
 					</div>
 					<p class="orange margin15">薪资 : {{ $job->salary }}</p>
-					<p class="gray margin15">工作经验 : {{ $job->experience }}</p>
+					<p class="gray margin15">工作经验要求 : {{ $job->experience }}</p>
 					<p class="gray margin15">发布时间 : {{ $job->updated_at }}</p>
 				</div>
 			</div>
