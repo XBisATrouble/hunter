4adf88a4f939ff719d647da63e28bc2a87909e7c
CrazyGrin <894355833@qq.com>
2017-05-19 23:06:48 +0800

    css

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -10,85 +10,181 @@
                 <li>查看面试</li>
             </ul>
         </div>
+
         <div class="main_company_right_banner">
-            <ul class="main_company_right_banner_nav">
-                <li>已收到的简历</li>
-                <li>已处理的简历</li>
-            </ul>
-            <div class="index_single_item">
-                <div class="index_single_item_jobinfo">
-                    <a class="gray big" href="">PHP工程师</a>
-                    <div class="index_single_item_jobinfo_numofpeople">
-                        收到
-                        <span class="numofpeople">5</span>
-                        份简历
+
+            <div class="jobs_modeul">
+                <ul class="main_company_right_banner_nav">
+                    <li>所有工作</li>
+                    <li>未收到简历的工作</li>
+                    <li>收到简历的工作</li>
+                </ul>
+                <div class="index_single_item">
+                    <div class="index_single_item_jobinfo">
+                        <a class="gray big" href="">PHP工程师</a>
+                        <div class="index_single_item_jobinfo_numofpeople">
+                            收到
+                            <span class="numofpeople">5</span>
+                            份简历
+                        </div>
+                        <div class="index_single_item_jobinfo_edit_button index_single_item_jobinfo_button">
+                            <a href="" class="full_a">查看简历</a>
+                        </div>
+                        <div class="index_single_item_jobinfo_offline_button index_single_item_jobinfo_button">
+                            <a href="" class="full_a">下线</a>
+                        </div>
+                        <a class="gray big" href=""></a>
+                        <p class="gray">
+                            工作发布于 :
+                            <br></p>
+                        <p class="gray" style="height: 100px;">
+                            工作简介 :
+                            <br></p>
                     </div>
-                    <div class="index_single_item_jobinfo_edit_button index_single_item_jobinfo_button">
-                        <a href="" class="full_a">查看简历</a>
-                    </div>
-                    <div class="index_single_item_jobinfo_offline_button index_single_item_jobinfo_button">
-                        <a href="" class="full_a">下线</a>
-                        <!-- ^^^^^^^^^^^^^^^^下线功能^^^^^^^^^^^^^^^^^^^ --> </div>
-                    <a class="gray big" href=""></a>
-                    <p class="gray">
-                        工作发布于 : 
-                        <br></p>
-                    <p class="gray" style="height: 100px;">
-                        工作简介 : 
-                        <br></p>
                 </div>
-            </div>
-            <div class="index_single_item">
-                <div class="index_single_item_jobinfo">
-                    <a class="gray big" href="">PHP工程师</a>
-                    <div class="index_single_item_jobinfo_numofpeople">
-                        收到
-                        <span class="numofpeople">5</span>
-                        份简历
+                <div class="index_single_item">
+                    <div class="index_single_item_jobinfo">
+                        <a class="gray big" href="">PHP工程师</a>
+                        <div class="index_single_item_jobinfo_numofpeople">
+                            收到
+                            <span class="numofpeople">5</span>
+                            份简历
+                        </div>
+                        <div class="index_single_item_jobinfo_edit_button index_single_item_jobinfo_button">
+                            <a href="" class="full_a">查看简历</a>
+                        </div>
+                        <div class="index_single_item_jobinfo_offline_button index_single_item_jobinfo_button">
+                            <a href="" class="full_a">下线</a>
+                        </div>
+                        <a class="gray big" href=""></a>
+                        <p class="gray">
+                            工作发布于 :
+                            <br></p>
+                        <p class="gray" style="height: 100px;">
+                            工作简介 :
+                            <br></p>
                     </div>
-                    <div class="index_single_item_jobinfo_edit_button index_single_item_jobinfo_button">
-                        <a href="" class="full_a">查看简历</a>
-                    </div>
-                    <div class="index_single_item_jobinfo_offline_button index_single_item_jobinfo_button">
-                        <a href="" class="full_a">下线</a>
-                        <!-- ^^^^^^^^^^^^^^^^下线功能^^^^^^^^^^^^^^^^^^^ --> </div>
-                    <a class="gray big" href=""></a>
-                    <p class="gray">
-                        工作发布于 : 
-                        <br></p>
-                    <p class="gray" style="height: 100px;">
-                        工作简介 : 
-                        <br></p>
                 </div>
             </div>
 
-                    <div class="admin_resume_thumb">
-            <div class="resume_base_thumb_pic">
-                <div class="resume_avatar_thumb"></div>
-            </div>
-            <div class="resume_base_thumb">
-                <p>
-                    <img src="/dist/resume_thumb/user.png">恽涵</p>
-                <p>
-                    <img src="/dist/resume_thumb/sch.png">重庆邮电大学</p>
-                <p>
-                    <img src="/dist/resume_thumb/age.png">20岁</p>
-            </div>
-            <div class="resume_base_thumb">
-                <p>
-                    <img src="/dist/resume_thumb/exp.png">一年项目经历</p>
-                <p>
-                    <img src="/dist/resume_thumb/major.png">软件工程专业</p>
-                <div class="resume_info_button">
-                    <a class="full_a" href="">查看简历</a>
+            <div class="resumes_modeul">
+                <ul class="main_company_right_banner_nav">
+                    <li>所有简历</li>
+                    <li>已收到的简历</li>
+                    <li>已处理的简历</li>
+                </ul>
+                <div class="admin_resume_thumb">
+                    <div class="resume_base_thumb_pic">
+                        <div class="resume_avatar_thumb"></div>
+                    </div>
+                    <div class="resume_base_thumb">
+                        <p>
+                            <img src="/dist/resume_thumb/user.png">恽涵</p>
+                        <p>
+                            <img src="/dist/resume_thumb/sch.png">重庆邮电大学</p>
+                        <p>
+                            <img src="/dist/resume_thumb/age.png">20岁</p>
+                    </div>
+                    <div class="resume_base_thumb">
+                        <p>
+                            <img src="/dist/resume_thumb/exp.png">一年项目经历</p>
+                        <p>
+                            <img src="/dist/resume_thumb/major.png">软件工程专业</p>
+                        <div class="resume_info_button">
+                            <a class="full_a" href="">查看简历</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="admin_resume_thumb">
+                    <div class="resume_base_thumb_pic">
+                        <div class="resume_avatar_thumb"></div>
+                    </div>
+                    <div class="resume_base_thumb">
+                        <p>
+                            <img src="/dist/resume_thumb/user.png">恽涵</p>
+                        <p>
+                            <img src="/dist/resume_thumb/sch.png">重庆邮电大学</p>
+                        <p>
+                            <img src="/dist/resume_thumb/age.png">20岁</p>
+                    </div>
+                    <div class="resume_base_thumb">
+                        <p>
+                            <img src="/dist/resume_thumb/exp.png">一年项目经历</p>
+                        <p>
+                            <img src="/dist/resume_thumb/major.png">软件工程专业</p>
+                        <div class="resume_info_button">
+                            <a class="full_a" href="">查看简历</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="admin_resume_thumb">
+                    <div class="resume_base_thumb_pic">
+                        <div class="resume_avatar_thumb"></div>
+                    </div>
+                    <div class="resume_base_thumb">
+                        <p>
+                            <img src="/dist/resume_thumb/user.png">恽涵</p>
+                        <p>
+                            <img src="/dist/resume_thumb/sch.png">重庆邮电大学</p>
+                        <p>
+                            <img src="/dist/resume_thumb/age.png">20岁</p>
+                    </div>
+                    <div class="resume_base_thumb">
+                        <p>
+                            <img src="/dist/resume_thumb/exp.png">一年项目经历</p>
+                        <p>
+                            <img src="/dist/resume_thumb/major.png">软件工程专业</p>
+                        <div class="resume_info_button">
+                            <a class="full_a" href="">查看简历</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="admin_resume_thumb">
+                    <div class="resume_base_thumb_pic">
+                        <div class="resume_avatar_thumb"></div>
+                    </div>
+                    <div class="resume_base_thumb">
+                        <p>
+                            <img src="/dist/resume_thumb/user.png">恽涵</p>
+                        <p>
+                            <img src="/dist/resume_thumb/sch.png">重庆邮电大学</p>
+                        <p>
+                            <img src="/dist/resume_thumb/age.png">20岁</p>
+                    </div>
+                    <div class="resume_base_thumb">
+                        <p>
+                            <img src="/dist/resume_thumb/exp.png">一年项目经历</p>
+                        <p>
+                            <img src="/dist/resume_thumb/major.png">软件工程专业</p>
+                        <div class="resume_info_button">
+                            <a class="full_a" href="">查看简历</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="admin_resume_thumb">
+                    <div class="resume_base_thumb_pic">
+                        <div class="resume_avatar_thumb"></div>
+                    </div>
+                    <div class="resume_base_thumb">
+                        <p>
+                            <img src="/dist/resume_thumb/user.png">恽涵</p>
+                        <p>
+                            <img src="/dist/resume_thumb/sch.png">重庆邮电大学</p>
+                        <p>
+                            <img src="/dist/resume_thumb/age.png">20岁</p>
+                    </div>
+                    <div class="resume_base_thumb">
+                        <p>
+                            <img src="/dist/resume_thumb/exp.png">一年项目经历</p>
+                        <p>
+                            <img src="/dist/resume_thumb/major.png">软件工程专业</p>
+                        <div class="resume_info_button">
+                            <a class="full_a" href="">查看简历</a>
+                        </div>
+                    </div>
                 </div>
             </div>
-            <div class="action_button_banner">
-                
-            </div>
-        </div>
-            <div class="line_gray"></div>
-            <div class="main_company_right_content"></div>
+
         </div>
     </div>
 </div>
