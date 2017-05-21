5e6f753068a3dca343411d656f4a2dc50b10c648
CrazyGrin <894355833@qq.com>
2017-05-21 22:58:12 +0800

    final

--- resources/views/admin/admin/index.blade.php
+++ resources/views/admin/admin/index.blade.php
@@ -1,4 +1,12 @@
 @extends('layouts.adminApp')
 @section('content')
 
+<div class="main_content">
+	<form class="company_info_edit">
+		<input type="text" name="company_name">
+		<textarea name="company_description"></textarea>
+		<button class="company_info_submit"></button>
+	</form>
+</div>
+
 @endsection
